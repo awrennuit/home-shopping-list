378059cb32c477c49e2b3877135dd3dfd26df4b0
AwrenNuit <a.rennaker@yahoo.com>
2020-10-16 21:51:34 -0500

    import AddItem and ItemList components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import AddItem from "../AddItem/AddItem";
+import ItemList from "../ItemList/ItemList";
 import "./App.css";
 
 export default function App() {
   return (
     <div>
-      <p>Operational</p>
+      <ItemList />
+      <AddItem />
     </div>
   );
 }
