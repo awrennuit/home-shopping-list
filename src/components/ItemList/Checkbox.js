88d901e129a6a9c441b23fdd93e5ff4f6e1ec4e8
AwrenNuit <a.rennaker@yahoo.com>
2020-10-17 14:42:03 -0500

    style checkboxes

--- src/components/ItemList/Checkbox.js
+++ src/components/ItemList/Checkbox.js
@@ -1,18 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../App/App";
+import "./Checkbox.css";
 
 export default function Checkbox(props) {
   const { state, dispatch } = useContext(Context);
-  const handleChange = (item) => dispatch({ type: `REMOVE_ITEM`, payload: item });
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    if (isChecked) {
+      // if timeout is happening, re-add item if item unchecked
+      // or add undo?
+      setTimeout(() => {
+        dispatch({ type: `REMOVE_ITEM`, payload: props.label });
+      }, 2000);
+    }
+  }, [isChecked]);
+
+  useEffect(() => {
+    setIsChecked(false);
+  }, [state.items]);
 
   return (
-    <label>
-      <input
-        type="checkbox"
-        checked={false}
-        onChange={() => handleChange(props.label)}
-      />
+    <label className={`checkbox-container ${isChecked ? "checked" : "unchecked"}`}>
       {props.label}
+      <input
+        checked={isChecked}
+        className="checkbox"
+        onChange={() => setIsChecked(!isChecked)}
+        type="checkbox"
+      />
+      <span className="checkbox-check"></span>
     </label>
   );
 }
