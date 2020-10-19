795ad769ef1c1bc93de46022c371c3c958bfd943
AwrenNuit <a.rennaker@yahoo.com>
2020-10-19 13:29:15 -0500

    hard-code db due to issues with env in deployment

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,8 @@ import "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL, // why does this not work when deployed?
+  databaseURL: "https://awren-shopping-list.firebaseio.com",
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
