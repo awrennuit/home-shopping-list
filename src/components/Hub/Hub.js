f72415de8a79a5b956e002e23c03a9c98971f4bf
AwrenNuit <a.rennaker@yahoo.com>
2020-10-23 13:39:12 -0500

    create component, add basic styling

---
+++ src/components/Hub/Hub.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hub.css";
+
+export default function Hub() {
+  
+  return (
+    <div id="hub-wrapper">
+      <p class="hub-heading">⋆｡*ﾟ✲**✲ﾟ*｡⋆</p>
+      <h1 class="hub-heading">Household Hub</h1>
+      <p class="hub-heading">⋆｡*ﾟ✲**✲ﾟ*｡⋆</p>
+    </div>
+  );
+}
