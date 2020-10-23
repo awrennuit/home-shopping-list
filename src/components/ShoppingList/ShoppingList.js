6bb88749014f7a7384afb4b441c769ebe5bea813
AwrenNuit <a.rennaker@yahoo.com>
2020-10-23 14:06:10 -0500

    rename components from Item to Shopping, add AddItem component to ShoppingList

---
+++ src/components/ShoppingList/ShoppingList.js
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect, useState } from "react";
+import AddItem from "../AddItem/AddItem";
+import { Context } from "../App/App";
+import ShoppingItem from "./ShoppingItem";
+
+export default function ShoppingList() {
+  const { state } = useContext(Context);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    if (state.items) setItems(Object.values(state.items));
+  }, [state.items]);
+
+  return (
+    <>
+      {items.length > 0 ? (
+        items.map((item, i) => (
+          <div key={i}>
+            <ShoppingItem label={item} />
+          </div>
+        ))
+      ) : (
+        <p>Nothing on the list yet</p>
+      )}
+      <hr />
+      <AddItem />
+    </>
+  );
+}
