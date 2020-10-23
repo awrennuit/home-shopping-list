83f36ad3b2c8ef21f67aecc6d6b2af431d1900f2
AwrenNuit <a.rennaker@yahoo.com>
2020-10-23 14:01:27 -0500

    add map for navigation

--- src/components/Hub/Hub.js
+++ src/components/Hub/Hub.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Hub.css";
+import Menu from "./Menu";
 
 export default function Hub() {
-  
+  const menuItems = [
+    {component: "ItemList", url: "/shopping"},
+    {component: "Recipes", url: "/recipes"}
+  ];
+
   return (
     <div id="hub-wrapper">
-      <p class="hub-heading">⋆｡*ﾟ✲**✲ﾟ*｡⋆</p>
-      <h1 class="hub-heading">Household Hub</h1>
-      <p class="hub-heading">⋆｡*ﾟ✲**✲ﾟ*｡⋆</p>
+      <p className="hub-heading">⋆｡*ﾟ✲**✲ﾟ*｡⋆</p>
+      <h1 className="hub-heading">Household Hub</h1>
+      <p className="hub-heading">⋆｡*ﾟ✲**✲ﾟ*｡⋆</p>
+      {menuItems.map((link, i) => (
+        <Fragment key={i}>
+          <Menu component={link.component} url={link.url} />
+        </Fragment>
+      ))}
     </div>
   );
 }
