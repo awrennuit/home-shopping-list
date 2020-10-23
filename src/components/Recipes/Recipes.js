e48ddcfd01a743f9f04b21b62dec764b82cdfccb
AwrenNuit <a.rennaker@yahoo.com>
2020-10-23 17:55:50 -0500

    add basic logic for rendering recipes with ingredients

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Fragment, useContext, useEffect, useState } from "react";
+import { Context } from "../App/App";
+import HomeButton from "../HomeButton/HomeButton";
 
 export default function Recipes() {
-  return(
+  const { state } = useContext(Context);
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    if (state.recipes) setRecipes(state.recipes);
+  }, [state.recipes]);
+
+  return (
     <>
+      <HomeButton />
+      <ul>
+        {Object.keys(recipes).map((name, i) => (
+          <Fragment key={i}>
+            <li>{name}</li>
+            <ul>
+              {Object.values(recipes[name]).map((ingredient, j) => (
+                <li key={j}>{ingredient}</li>
+              ))}
+            </ul>
+          </Fragment>
+        ))}
+      </ul>
+      <hr />
+      <p>ADD RECIPES HERE</p>
     </>
   );
 }
