dea984c4a4edccc3fd51a5202aa826b3cd23cf38
AwrenNuit <a.rennaker@yahoo.com>
2020-10-23 15:44:09 -0500

    create component, style button

---
+++ src/components/HomeButton/HomeButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./HomeButton.css";
+
+export default function HomeButton() {
+  const history = useHistory();
+
+  return (
+    <div>
+      <button id="home-btn" onClick={() => history.push("/")}>
+        HOME
+      </button>
+    </div>
+  );
+}
