a60ffb4c1568f3856944cc2c4ac4224e818ab998
AwrenNuit <a.rennaker@yahoo.com>
2020-11-29 15:56:52 -0600

    update shopping endpoint, add url/endpoint/reducer/component for Birthdays

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,30 +3,37 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { db } from "../../firebase";
 import "./App.css";
 import AddRecipe from "../AddRecipe/AddRecipe";
+import BirthdayTracker from "../BirthdayTracker/BirthdayTracker";
+import Chores from "../Chores/Chores";
 import Hub from "../Hub/Hub";
 import Recipes from "../Recipes/Recipes";
 import ShoppingList from "../ShoppingList/ShoppingList";
+import ThisRecipe from "../ThisRecipe/ThisRecipe";
 import Wishlist from "../Wishlist/Wishlist";
-import Chores from "../Chores/Chores";
-import BirthdayTracker from "../BirthdayTracker/BirthdayTracker";
 
 export const Context = React.createContext();
 
 const initialState = {
+  birthdays: [],
   chores: [],
   recipes: {},
   shoppingList: [],
+  thisRecipe: [],
   wishlist: [],
 };
 
 const contextReducer = (state, action) => {
   switch (action.type) {
+    case `SET_BIRTHDAYS`:
+      return { ...state, birthdays: action.payload };
     case `SET_CHORES`:
       return { ...state, chores: action.payload };
     case `SET_RECIPES`:
       return { ...state, recipes: action.payload };
     case `SET_SHOPPING_LIST`:
       return { ...state, shoppingList: action.payload };
+    case `SET_THIS_RECIPE`:
+      return { ...state, thisRecipe: action.payload };
     case `SET_WISHLIST`:
       return { ...state, wishlist: action.payload };
     default:
@@ -43,6 +50,7 @@ export default function App() {
 
   const fetchData = async () => {
     const dataToStore = [
+      { url: "birthdays/", type: "SET_BIRTHDAYS" },
       { url: "chores/", type: "SET_CHORES" },
       { url: "recipes/", type: "SET_RECIPES" },
       { url: "shopping/", type: "SET_SHOPPING_LIST" },
@@ -63,10 +71,11 @@ export default function App() {
     <Context.Provider value={{ state, dispatch }}>
       <Router>
         <Route exact path="/" component={Hub} />
-        <Route exact path="/add-recipe" component={AddRecipe} />
         <Route exact path="/birthdays" component={BirthdayTracker} />
         <Route exact path="/chores" component={Chores} />
         <Route exact path="/recipes" component={Recipes} />
+        <Route exact path="/recipes/:id" component={ThisRecipe} />
+        <Route exact path="/recipes/add" component={AddRecipe} />
         <Route exact path="/shopping" component={ShoppingList} />
         <Route exact path="/wishlist" component={Wishlist} />
       </Router>
